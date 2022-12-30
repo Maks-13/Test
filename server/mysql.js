2cc8f394c0a2873967aa7acaf7839d9db1948d83
Maxim Yunaev <max01_13@mail.ru>
2022-12-30 15:33:13 +0300

    Second commit

---
+++ server/mysql.js
@@ -0,0 +1,39 @@
+
+const mysql = require("mysql2");
+let fs = require("fs");
+
+
+let obj;
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "HockeyShop",
+    password: "Pineda2012"
+})
+
+const meth = connection.connect(function(err){
+    if(err){
+        return console.error("Ошибка: " + err.message);
+    }else{
+        console.log("Подключение к серверу MySQL успешно установлено");
+    }
+})
+
+const sql = `SELECT * FROM kindsitems`;
+
+
+connection.query(sql, function(err, results) {
+    if(err) console.log(err);
+    let data = JSON.stringify(results);
+    fs.writeFile('../DataJson.json', data, 'utf-8', function(err){
+        if(err) return console.log(err);
+        console.log("Файл DataJson теперь содержит данные бд!");
+    }); 
+    console.log(results);
+});
+ 
+connection.end();
+
+
---
+++ server/server.js
@@ -0,0 +1,19 @@
+/* let cors = require('cors'); */
+require('./mysql.js');
+const fs = require('fs');
+const express = require("express");
+// создаем приложение
+const app = express();
+/*  app.use(cors); */
+// устанавливаем обработчик для маршрута "/"
+app.get("/", async (request, response) => {
+    fs.readFile('../DataJson.json', 'utf8', (err, data) => {
+        if (err) return console.log('File read failed: ', err);
+        response.send(data);
+    });
+});
+
+// начинаем прослушивание подключений на 3000 порту
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,console.log(`Server started on port ${PORT}`));
